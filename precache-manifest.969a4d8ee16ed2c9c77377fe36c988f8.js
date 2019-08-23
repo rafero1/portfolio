262544be2cb34d4b14b259898d75cf87f0fef881
rafero1 <rfl.avlar@outlook.com>
2019-08-23 15:06:40 -0300

    Updates

--- precache-manifest.969a4d8ee16ed2c9c77377fe36c988f8.js
+++ precache-manifest.969a4d8ee16ed2c9c77377fe36c988f8.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "24e191f31db5f39da8f2ce6e8dac6e19",
+    "revision": "c16f85dba82531094de78179d955d652",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "4ef20dd82538055f48b5",
-    "url": "/portfolio/static/css/main.7a23ed7a.chunk.css"
+    "revision": "1bc53267623ced743ea0",
+    "url": "/portfolio/static/css/main.52ac983a.chunk.css"
   },
   {
     "revision": "8c188acbf97b1862f8b1",
     "url": "/portfolio/static/js/2.56e74f20.chunk.js"
   },
   {
-    "revision": "4ef20dd82538055f48b5",
-    "url": "/portfolio/static/js/main.e5e1371b.chunk.js"
+    "revision": "1bc53267623ced743ea0",
+    "url": "/portfolio/static/js/main.afccd86a.chunk.js"
   },
   {
     "revision": "53ebc025fca807d4dbea",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.a8625243017bceb4c50f3ebe09ac413e.js"
+  "/portfolio/precache-manifest.969a4d8ee16ed2c9c77377fe36c988f8.js"
 );
 
 self.addEventListener('message', (event) => {
