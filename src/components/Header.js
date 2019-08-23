1b387bb42062225c3a1e64967f133bf89da61ce1
rafero1 <rfl.avlar@outlook.com>
2019-08-23 14:58:18 -0300

    Modified layout and text content

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,10 +10,9 @@ export class Header extends Component {
                         <div className="col">
                             <img className="site-logo" src="./logo@2x.png" alt="Rafael Avilar portfólio" />
                             <div className="content">
-                                <p><span className="is-tinted">Olá!</span> Meu nome é <span className="is-tinted">Rafael</span>, e atualmente estou estudando <span className="is-tinted">Sistemas e Mídias Digitais</span> na <span className="is-tinted">UFC</span> de Fortaleza.</p>
-                                <p>Primariamente trabalho com <span className="is-tinted">webdev</span> no <span className="is-tinted">front-end</span>, <span className="is-tinted">WordPress</span> e também estou aprendendo <span className="is-tinted">back</span>. Logo abaixo de web, minha segunda área de maior experiência é <span className="is-tinted">mobile</span>, especialmente <span className="is-tinted">android nativo</span>.</p>
-                                <p>Estou construindo esse site para servir de <span className="is-tinted">portfólio</span> e mostrar alguns dos projetos que desenvolvi até então.</p>
-                                <p>Mais projetos estão à caminho ;)</p>
+                                <p><span className="is-tinted">Olá!</span> Meu nome é <span className="is-tinted">Rafael</span>, e sou um estudante de <span className="is-tinted">Sistemas e Mídias Digitais</span> na <span className="is-tinted">Universidade Federal do Ceará</span>, em Fortaleza.</p>
+                                <p>Minhas áreas primárias são <span className="is-tinted">Desenvolvimento Mobile</span> e <span className="is-tinted">Web (Front e Back)</span>.</p>
+                                <p>Abaixo poderá encontrar alguns exemplos de projetos que desenvolvi.</p>
                             </div>
                             <nav>
                                 <Button text="Projetos" goto="#projects"></Button>
